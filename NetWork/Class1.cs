9525f4a4e40f64ef2da77c2e5c3f02bf61f5494d
hiyforever <hiyforever@users.noreply.github.com>
2021-02-03 01:25:44 +0800

    Add files via upload

---
+++ NetWork/Class1.cs
@@ -0,0 +1,30 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpDeskBand;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NetWork
+{
+    [ComVisible(true)]
+    [DisplayName("网速")]
+    public class Class1 : SharpDeskBand
+    {
+        protected override UserControl CreateDeskBand()
+        {
+            return new UserControl1();
+        }
+
+        protected override BandOptions GetBandOptions()
+        {
+            return new BandOptions
+            {
+                HasVariableHeight = false,
+                IsSunken = false,
+                ShowTitle = true,
+                Title = "NetWork",
+                UseBackgroundColour = false,
+                AlwaysShowGripper = true
+            };
+        }
+    }
+}
---
+++ NetWork/bin/Release/Installer.bat
@@ -0,0 +1,13 @@
+@echo off
+
+title Desk Band Debug Installer
+
+SET path_dll=D:\hiyfo\Documents\c#\NetWork\NetWork\bin\Release\NetWork.dll
+SET path_regasm=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe
+
+"%path_regasm%" /codebase %path_dll%
+
+taskkill /im explorer.exe /f
+start explorer.exe
+
+pause
