37e5e5dd58dc16e051d0c3ac0fd70d70581df706
hiyforever <hiyforever@users.noreply.github.com>
2021-09-19 23:46:40 +0800

    Add files via upload

---
+++ NetWork/UserControl1.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NetWork
+{
+    partial class UserControl1
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private Label label1;
+        private Label label2;
+        private Timer timer1;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.label1 = new Label();
+            this.label2 = new Label();
+            this.timer1 = new Timer(this.components);
+            this.SuspendLayout();
+            this.label1.Location = new Point(0, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(75, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0k/s";
+            this.label1.TextAlign = ContentAlignment.MiddleRight;
+            this.label2.Location = new Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new Size(75, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0k/s";
+            this.label2.TextAlign = ContentAlignment.MiddleRight;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new EventHandler(this.Timer1_Tick);
+            this.AutoScaleDimensions = new SizeF(9f, 15f);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = Color.Black;
+            this.Controls.Add((Control)this.label2);
+            this.Controls.Add((Control)this.label1);
+            this.Font = new Font("宋体", 9f, FontStyle.Bold, GraphicsUnit.Point, (byte)134);
+            this.Name = "UserControl1";
+            this.Size = new Size(78, 50);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ NetWork/UserControl1.cs
@@ -0,0 +1,65 @@
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace NetWork
+{
+    public partial class UserControl1 : UserControl
+    {
+        private readonly PerformanceCounterCategory pcc = new PerformanceCounterCategory("Network Interface");
+        private readonly Dictionary<string, PerformanceCounter>[] devices;
+        private readonly Label[] labels;
+        private readonly string[] counterNames;
+
+        public UserControl1()
+        {
+            this.InitializeComponent();
+            this.labels = new Label[2] { this.label1, this.label2 };
+            this.counterNames = new string[2]
+            {
+                "Bytes Received/sec",
+                "Bytes Sent/sec"
+            };
+            this.devices = new Dictionary<string, PerformanceCounter>[2]
+            {
+        new Dictionary<string, PerformanceCounter>(),
+        new Dictionary<string, PerformanceCounter>()
+            };
+            this.timer1.Start();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            float[] numArray = new float[this.counterNames.Length];
+            foreach (string instanceName in this.pcc.GetInstanceNames())
+            {
+                for (int index = 0; index < this.counterNames.Length; ++index)
+                {
+                    if (!this.devices[index].ContainsKey(instanceName))
+                        this.devices[index][instanceName] = new PerformanceCounter(this.pcc.CategoryName, this.counterNames[index], instanceName);
+                    try
+                    {
+                        numArray[index] += this.devices[index][instanceName].NextValue() / 1024f;
+                    }
+                    catch
+                    {
+                        this.devices[index][instanceName].Close();
+                        this.devices[index].Remove(instanceName);
+                    }
+                }
+            }
+            string[] strArray = new string[2] { "k/s", "k/s" };
+            for (int index = 0; index < numArray.Length; ++index)
+            {
+                if ((double)numArray[index] >= 999.5)
+                {
+                    numArray[index] /= 1024f;
+                    strArray[index] = "m/s";
+                }
+                this.labels[index].Text = Convert.ToDouble(numArray[index].ToString((double)numArray[index] < 0.1 ? "0.00" : ((double)numArray[index] < 10.0 ? "0.0" : "G3"))).ToString() + strArray[index];
+            }
+        }
+    }
+}
