fba51fb59ed9fa06d5c1d597343f753dbfc16de6
hiyforever <hiyforever@users.noreply.github.com>
2021-09-23 22:50:44 +0800

    Update Uninstaller.bat

--- NetWork/bin/Release/Uninstaller.bat
+++ NetWork/bin/Release/Uninstaller.bat
@@ -2,7 +2,7 @@
 
 title Desk Band Debug Uninstaller
 
-SET path_dll=D:\hiyfo\Documents\c#\NetWork\NetWork\bin\Release\NetWork.dll
+SET path_dll=%cd%\NetWork.dll
 SET path_regasm=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe
 
 "%path_regasm%" /u %path_dll%
